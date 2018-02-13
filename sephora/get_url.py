e79b83a6acd56e31101f04b389b0b04285caa1b4
Ryu Taehwan <yth3260@gmail.com>
2018-02-13 23:30:44 +0900

    sephora 스크롤 내리기 완성

--- sephora/get_url.py
+++ sephora/get_url.py
@@ -1,8 +1,23 @@
 import requests
+import time
 from bs4 import BeautifulSoup as bs
 from selenium import webdriver
+
 
 def get_url():
     driver = webdriver.Firefox()
     driver.get('https://www.sephora.com/best-selling-makeup')
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    SCROLL_PAUSE_TIME = 0.5
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    while True:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(SCROLL_PAUSE_TIME)
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_height
+
+
+if __name__ == '__main__':
+    get_url()
